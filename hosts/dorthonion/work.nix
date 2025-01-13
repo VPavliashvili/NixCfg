e32dd0c4d906cda18d6b77d467b10d9a2862a9c9
VPavliashvili <v_pavliashvili@yahoo.com>
2025-01-13 21:13:31 +0400

    openconnect added to nopassword on dorthonion

--- hosts/dorthonion/work.nix
+++ hosts/dorthonion/work.nix
@@ -1,5 +1,9 @@
-{ lib, config, pkgs, ... }:
 {
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ../common/modules/work
   ];
@@ -8,5 +12,24 @@
     vpn.enable = true;
     addRemmina = true;
     addTeams = true;
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = ["stranger"];
+      commands = [
+        {
+          command = "${pkgs.openconnect}/bin/openconnect";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
+  security.wrappers.openconnect = {
+    source = "${pkgs.openconnect}/bin/openconnect";
+    owner = "root";
+    group = "root";
+    capabilities = "cap_net_admin+ep";
   };
 }
