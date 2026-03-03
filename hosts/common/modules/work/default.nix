fb7d1987eaff1d843e327e3f128fa3694d894bea
VPavliashvili <v_pavliashvili@yahoo.com>
2026-03-03 07:36:57 +0400

    no root for openconnect refactored out

--- hosts/common/modules/work/default.nix
+++ hosts/common/modules/work/default.nix
@@ -23,6 +23,31 @@ in {
       default = false;
       description = "install teams for linux";
     };
+    noroot = mkOption {
+      type = types.bool;
+      default = true;
+      description = "avoid openconnect to require root permission(used for my custom work vpn connection script)";
+    };
+  };
+
+  config.security = mkIf cfg.noroot {
+    sudo.extraRules = [
+      {
+        users = [mainUser];
+        commands = [
+          {
+            command = "${pkgs.openconnect}/bin/openconnect";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+    wrappers.openconnect = {
+      source = "${pkgs.openconnect}/bin/openconnect";
+      owner = "root";
+      group = "root";
+      capabilities = "cap_net_admin+ep";
+    };
   };
 
   config.environment.systemPackages =
--- hosts/dorthonion/work.nix
+++ hosts/dorthonion/work.nix
@@ -13,24 +13,6 @@
     vpn.enable = true;
     addRemmina = true;
     addTeams = true;
-  };
-
-  security.sudo.extraRules = [
-    {
-      users = [mainUser];
-      commands = [
-        {
-          command = "${pkgs.openconnect}/bin/openconnect";
-          options = ["NOPASSWD"];
-        }
-      ];
-    }
-  ];
-
-  security.wrappers.openconnect = {
-    source = "${pkgs.openconnect}/bin/openconnect";
-    owner = "root";
-    group = "root";
-    capabilities = "cap_net_admin+ep";
+    noroot = true;
   };
 }
